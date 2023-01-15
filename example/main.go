2335e4a5b68a8e970f93fd628acf57aa5c2e38a3
uang <kpengfly@gmail.com>
2023-01-15 15:29:43 +0800

    删除无用的代码

--- example/main.go
+++ example/main.go
@@ -20,33 +20,30 @@ func main() {
 	fileTypeObj := filetype.NewFileType()
 	// 获取文件类型
 	if fileTypeObj.GetFileType(GZ_PATH) != filetype.FILE_TYPE_GZ {
-		log.Fatal("fileype should be gz")
+		log.Fatal("file type should be gz")
 	}
 	if fileTypeObj.GetFileType(TGZ_PATH) != filetype.FILE_TYPE_TGZ {
-		log.Fatal("fileype should be tgz")
+		log.Fatal("file type should be tgz")
 	}
 	if fileTypeObj.GetFileType(ZIP_PATH) != filetype.FILE_TYPE_ZIP {
-		log.Fatal("fileype should be zip")
+		log.Fatal("file type should be zip")
 	}
 	if fileTypeObj.GetFileType(TAR_PATH) != filetype.FILE_TYPE_TAR {
-		log.Fatal("fileype should be tar")
+		log.Fatal("file type should be tar")
 	}
 	// 传入非二进制文件，进行类型判断
 	if fileTypeObj.GetFileType(JSON_PATH) != filetype.FILE_TYPE_JSON {
-		log.Fatal("fileype should be json")
+		log.Fatal("file type should be json")
 	}
 	// 判断文件类型
 	if !fileTypeObj.CheckFileType(GZ_PATH, filetype.FILE_TYPE_GZ) {
-		log.Fatal("file Type should be gz")
+		log.Fatal("file type should be gz")
 	}
 	if !fileTypeObj.CheckFileType(TGZ_PATH, filetype.FILE_TYPE_TGZ) {
-		log.Fatal("file Type should be tgz")
+		log.Fatal("file type should be tgz")
 	}
 	if !fileTypeObj.CheckFileType(TAR_GZ_PATH, filetype.FILE_TYPE_TAR_GZ) {
-		log.Fatal("file Type should be tar.gz")
-	}
-	if !fileTypeObj.CheckFileType("D:/test/tgz/code.tgz", filetype.FILE_TYPE_GZ) {
-		log.Fatal("file Type should be gz")
+		log.Fatal("file type should be tar.gz")
 	}
 	if !fileTypeObj.CheckFileType(ZIP_PATH, filetype.FILE_TYPE_ZIP) {
 		log.Fatal("file Type should be zip")
